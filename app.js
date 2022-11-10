141102b0a4a00ebf4e2514ccad8aa59a7c3110bc
iloniemi <juho.iloniemi@gmail.com>
2022-11-10 23:32:13 +0200

    kontrolleri nyt mukana. 4-2 tehty

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-const { info } = require('./utils/logger')
+const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { MONGODB_URI } = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 
-info('Connecting to database')
+logger.info('Connecting to database')
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
-    info('Connected to the database')
+    logger.info('Connected to the database')
   })
   .catch(error => {
-    info('Error connecting to the database', error.message)
+    logger.error('Error connecting to the database', error.message)
   })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,55 +1,10 @@
-require('dotenv').config()
-const {info, error} = require('./utils/logger')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const { info } = require('./utils/logger')
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const http = require('http')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-
-
-//'mongodb://localhost/bloglist'
-info('Connecting to database')
-
-mongoose.connect(mongoUrl)
-  .then(() => {
-    info('Connected to the database')
-  })
-  .catch(error => {
-    info('Error connecting to the database', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   info(`Server running on port ${PORT}`)
 })
